61924883e3ac37df7cc0a0b40c26991a0a6bf68d
AGO 1011 <abdul.gofur@1011.com>
2018-11-24 19:52:45 +0700

    updated port mysql

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const db = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
     password : 'root',
-    port: 8889,
+    port: 3306,
     database:'temandig_sakila'
 });
 
