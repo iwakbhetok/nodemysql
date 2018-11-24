5f7fe8a2d87f7ebadf1c5a6fba3136d83e0eddae
AGO 1011 <abdul.gofur@1011.com>
2018-11-24 20:02:18 +0700

    updated

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const mysql = require('mysql');
 // Create connection
 const db = mysql.createConnection({
     host     : 'localhost',
-    user     : 'temandig_ago',
-    password : '31121984createit',
+    user     : 'temandig_test',
+    password : 'QWERTY!@#$%^',
     port: 3306,
     database:'temandig_sakila'
 });
